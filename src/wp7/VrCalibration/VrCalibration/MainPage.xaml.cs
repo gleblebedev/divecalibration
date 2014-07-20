d97e2d96e729afc6742abf84af23e14cda87f058
Gleb Lebedev <gleb@gleblebedev.com>
2014-07-20 23:56:09 +0100

    Prototype version

---
+++ src/wp7/VrCalibration/VrCalibration/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+
+namespace VrCalibration
+{
+	public partial class MainPage : PhoneApplicationPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+			Microsoft.Phone.Shell.PhoneApplicationService.Current.UserIdleDetectionMode = IdleDetectionMode.Enabled;
+		}
+
+		private void StartCalibration(object sender, RoutedEventArgs e)
+		{
+			NavigationService.Navigate(new Uri("/GamePage.xaml", UriKind.Relative));
+		}
+	}
+}
